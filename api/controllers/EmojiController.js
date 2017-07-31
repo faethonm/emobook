c1f7d9ae0f525fc39f164f0694c570d0658d2a87
Faethon Milikouris <faethon.milikouris@gmail.com>
2017-07-30 17:47:58 -0700

    Set up mongodb

--- api/controllers/EmojiController.js
+++ api/controllers/EmojiController.js
@@ -6,6 +6,11 @@
  */
 
 module.exports = {
+  // find: function(req, reds){},
+  // findOne: function(req, reds){},
+  // create: function(req, reds){},
+  // update: function(req, reds){},
+  // destroy: function(req, reds){}
 	
 };
 
